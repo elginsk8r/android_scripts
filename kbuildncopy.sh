628bb2998730a407c4bd4312a4507ff658ce5786
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-05-27 01:23:23 -0500

    kernel: add error checking

--- kbuildncopy.sh
+++ kbuildncopy.sh
@@ -3,14 +3,12 @@
 # build and copy kernels
 
 if [ $# -eq 0 ]; then
-	echo "ERROR: must specify board(s)"; exit 1;
+	echo "ERROR: must specify board(s) (`basename $0`-h)"; exit 1;
 fi
 
 if [ -z $CROSS_COMPILE ]; then
 	echo "ERROR: you need to set your environment"; exit 1;
 fi
-
-sourcedir=/backups/android/evervolv
 
 passion=0
 bravo=0
@@ -21,14 +19,14 @@ get_help()
 {
 cat <<EOF
 Usage:
-	p = passion
-	b = bravo
-	s = supersonic
-	i = inc
-	t = use tiamat config
-	c = set config
-	d = set destination directory
-	h = show this
+	p : passion
+	b : bravo
+	s : supersonic
+	i : inc
+	t : use tiamat config
+	c : set config
+	d : set destination directory
+	h : show this
 EOF
 }
 
@@ -48,47 +46,57 @@ done
 if [ -z "$config" ]; then
 	config=evervolv
 fi
+if [ -z "$sourcedir" ]; then
+	sourcedir=/backups/android/evervolv
+fi
+
+# Takes 1 arg: defconfig
+build() {
+	echo BUILDING $1
+	make mrproper
+	make $1
+	time schedtool -B -n 1 -e ionice -n 1 make -j 6
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+}
+
+# Takes 2 args: source, destination
+copy() {
+	cp -v $1 $2
+	if [ $? -ne 0 ]; then
+		exit 1
+	fi
+}
 
 if [ $passion -eq 1 ]; then
-	echo BUILDING ${config}_mahimahi_defconfig
-	make mrproper
-	make ${config}_mahimahi_defconfig
-	time schedtool -B -n 1 -e ionice -n 1 make -j 6
-	cp -v drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/passion/prebuilt/bcm4329.ko
-	cp -v arch/arm/boot/zImage $sourcedir/device/htc/passion/prebuilt/kernel
+	build ${config}_mahimahi_defconfig
+	copy drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/passion/prebuilt/bcm4329.ko
+	copy arch/arm/boot/zImage $sourcedir/device/htc/passion/prebuilt/kernel
 	echo DONE ${config}_mahimahi_defconfig
 fi
 
 if [ $bravo -eq 1 ]; then
-	echo BUILDING ${config}_bravo_defconfig
-	make mrproper
-	make ${config}_bravo_defconfig
-	time schedtool -B -n 1 -e ionice -n 1 make -j 6
-	cp -v drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/bravo/prebuilt/bcm4329.ko
-	cp -v arch/arm/boot/zImage $sourcedir/device/htc/bravo/prebuilt/kernel
+	build ${config}_bravo_defconfig
+	copy drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/bravo/prebuilt/bcm4329.ko
+	copy arch/arm/boot/zImage $sourcedir/device/htc/bravo/prebuilt/kernel
 	echo DONE ${config}_bravo_defconfig
 fi
 
 if [ $ss -eq 1 ]; then
-	echo BUILDING ${config}_supersonic_defconfig
-	make mrproper
-	make ${config}_supersonic_defconfig
-	time schedtool -B -n 1 -e ionice -n 1 make -j 6
-	cp -v drivers/net/wimax/SQN/sequans_sdio.ko $sourcedir/device/htc/supersonic/modules/sequans_sdio.ko
-	cp -v drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/supersonic/modules/bcm4329.ko
-	cp -v drivers/net/wimax/wimaxdbg/wimaxdbg.ko $sourcedir/device/htc/supersonic/modules/wimaxdbg.ko
-	cp -v drivers/net/wimax/wimaxuart/wimaxuart.ko $sourcedir/device/htc/supersonic/modules/wimaxuart.ko
-	cp -v arch/arm/boot/zImage $sourcedir/device/htc/supersonic/prebuilt/root/kernel
+	build ${config}_supersonic_defconfig
+	copy drivers/net/wimax/SQN/sequans_sdio.ko $sourcedir/device/htc/supersonic/modules/sequans_sdio.ko
+	copy drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/supersonic/modules/bcm4329.ko
+	copy drivers/net/wimax/wimaxdbg/wimaxdbg.ko $sourcedir/device/htc/supersonic/modules/wimaxdbg.ko
+	copy drivers/net/wimax/wimaxuart/wimaxuart.ko $sourcedir/device/htc/supersonic/modules/wimaxuart.ko
+	copy arch/arm/boot/zImage $sourcedir/device/htc/supersonic/prebuilt/root/kernel
 	echo DONE ${config}_supersonic_defconfig
 fi
 
 if [ $inc -eq 1 ]; then
-	echo BUILDING ${config}_incrediblec_defconfig
-	make mrproper
-	make ${config}_incrediblec_defconfig
-	time schedtool -B -n 1 -e ionice -n 1 make -j 6
-	cp -v drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/inc/prebuilt/lib/modules/bcm4329.ko
-	cp -v drivers/net/ifb.ko $sourcedir/device/htc/inc/prebuilt/lib/modules/ifb.ko
-	cp -v arch/arm/boot/zImage $sourcedir/device/htc/inc/prebuilt/root/kernel
+	build ${config}_incrediblec_defconfig
+	copy drivers/net/wireless/bcm4329/bcm4329.ko $sourcedir/device/htc/inc/prebuilt/lib/modules/bcm4329.ko
+	copy drivers/net/ifb.ko $sourcedir/device/htc/inc/prebuilt/lib/modules/ifb.ko
+	copy arch/arm/boot/zImage $sourcedir/device/htc/inc/prebuilt/root/kernel
 	echo DONE ${config}_incrediblec_defconfig
 fi
