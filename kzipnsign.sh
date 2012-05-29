edb50a7e676628745f833d38e75040afadc2cedf
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-05-29 14:41:53 -0500

    kzipnsign: generate updater-script so a custom message can be passed

--- kzipnsign.sh
+++ kzipnsign.sh
@@ -14,9 +14,11 @@ case $1
 in
 	p)
 	name=test-kernel-drewis-n1
+	updatemessage="NexusOne test kernel by drewis"
 	;;
 	b)
 	name=test-kernel-drewis-desire
+	updatemessage="HTCDesire test kernel by drewis"
 	;;
 	*)
 	echo fuck you ; exit 1
@@ -31,6 +33,8 @@ zdir=test-kernels
 testsignloc=/usr/bin/testsign.jar
 # name of zip file (minus .zip) defaults to YearMonthDay-Hour-Min
 zipname=${name}-`date +%Y%m%d-%H-%M`
+# updater-script location
+updaterscript=META-INF/com/google/android/updater-script
 
 if [ ! -e $testsignloc ]; then
 	echo Cant find testsign.jar; exit 1
@@ -64,6 +68,34 @@ else
 fi
 
 cd $kdir/$name
+
+echo creating updater-script..
+cat > $updaterscript <<EOF
+ui_print("");
+ui_print("$updatemessage");
+ui_print("");
+ui_print("AnyKernel Updater by Koush.");
+ui_print("");
+ui_print("Extracting System Files...");
+set_progress(1.000000);
+mount("MTD", "system", "/system");
+package_extract_dir("system", "/system");
+set_perm_recursive(0, 0, 0755, 0644, "/system/lib/modules");
+unmount("/system");
+ui_print("Extracting Kernel files...");
+package_extract_dir("kernel", "/tmp");
+ui_print("Installing kernel...");
+set_perm(0, 0, 0777, "/tmp/dump_image");
+set_perm(0, 0, 0777, "/tmp/mkbootimg.sh");
+set_perm(0, 0, 0777, "/tmp/mkbootimg");
+set_perm(0, 0, 0777, "/tmp/unpackbootimg");
+run_program("/tmp/dump_image", "boot", "/tmp/boot.img");
+run_program("/tmp/unpackbootimg", "/tmp/boot.img", "/tmp/");
+run_program("/tmp/mkbootimg.sh");
+write_raw_image("/tmp/newboot.img", "boot");ui_print("");
+ui_print("Done!");
+ui_print("");
+EOF
 
 if [ "`pwd`" != "$kdir/$name" ]; then
 	echo change directory failed; exit 1
