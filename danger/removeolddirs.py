8b99a4b75b805c89ab3b782e45cbec3d9d1e5d78
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-09 00:19:37 -0500

    accept the path as arg in removeolddirs

--- danger/removeolddirs.py
+++ danger/removeolddirs.py
@@ -1,18 +1,19 @@
+#!/usr/bin/env python
 '''
     Yanked from stackoverflow
     delete subdirs in directories older than numdays
     To be used for nightly builds to only keep the latest
+    arg 1: directory to check
 '''
 
+from sys import argv
 import os
 import time
 import shutil
 
 numdays = 86400*14 # 2 weeks
 now = time.time()
-# TODO: parse as argv
-directory=os.path.join("/home","drew","uploads","cron")
-
+script, directory = argv
 for r,d,f in os.walk(directory):
     for dir in d:
          timestamp = os.path.getmtime(os.path.join(r,dir))
