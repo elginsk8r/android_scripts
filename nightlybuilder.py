658d2f9397acb373e009c64cb58f305593dd447e
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-17 02:48:52 -0500

    nightlybuilder: upload and mirror builds with python

--- nightlybuilder.py
+++ nightlybuilder.py
@@ -11,7 +11,7 @@ import subprocess
 from drewis import html
 
 
-VERSION = '0.2'
+VERSION = '0.3'
 NIGHTLY_SCRIPT_DIR = os.path.join(os.path.dirname(os.path.realpath(__file__)), 'nightly')
 DATE = datetime.datetime.now().strftime('%Y.%m.%d')
 
@@ -40,7 +40,6 @@ if os.path.isdir(buildlogdir) == False:
 logfile = os.path.join(buildlogdir, 'buildlog-' + DATE + '.log')
 htmllogfile = os.path.join(buildlogdir, 'buildlog-' + DATE + '.html')
 os.putenv('EV_BUILDLOG', logfile)
-os.putenv('EV_HTML_BUILDLOG', htmllogfile)
 
 # changelog
 changelogdir = os.path.join(os.path.realpath(os.getcwd()), 'nightly_changelogs')
@@ -49,11 +48,12 @@ if os.path.isdir(changelogdir) == False:
 changelogfile = os.path.join(changelogdir, 'changelog-' + DATE + '.log')
 htmlchangelogfile = os.path.join(changelogdir, 'changelog-' + DATE + '.html')
 os.putenv('EV_CHANGELOG', changelogfile)
-os.putenv('EV_HTML_CHANGELOG', htmlchangelogfile)
 
 # upload path
 uploadpath = os.path.join('~', 'uploads', 'cron', DATE)
-os.putenv('EV_UPLOAD_PATH', uploadpath)
+
+# mirror path (append to localmirror)
+mirrorpath = os.path.join('cron', DATE)
 
 # pull common env variables
 droiduser = os.getenv('DROID_USER')
@@ -67,8 +67,21 @@ if args.nosync == False:
 # build each target
 for target in args.target:
     os.putenv('EV_NIGHTLY_TARGET', target)
+    # Run the build: target will be pulled from env
     subprocess.call([os.path.join(NIGHTLY_SCRIPT_DIR, 'build.sh')], shell=True)
-    subprocess.call([os.path.join(NIGHTLY_SCRIPT_DIR, 'upload.sh')], shell=True)
+    # find, upload and mirror the zips
+    # TODO: copy the files out and upload asyncronously while continuing to
+    #       build other targets
+    zips = []
+    for files in os.listdir(os.path.join('out', 'target', 'product', target)):
+        if files.startswith('Evervolv') and files.endswith('.zip'):
+            zips.append(files)
+    if zips and droiduser and droidhost:
+        for zipfile in zips:
+            subprocess.call(['rsync', '-P', zipfile, droiduser + '@' + droidhost + ':' + uploadpath ])
+    if zips and localmirror:
+        for zipfile in zips:
+            subprocess.call(['rsync', '-P', zipfile, os.path.join(localmirror, mirrorpath)])
 
 # create html changelog
 if os.path.exists(changelogfile):
@@ -87,11 +100,10 @@ if os.path.exists(logfile):
     bl.write(htmllogfile)
 # upload the html files
 if droiduser and droidhost:
-    subprocess.call([ 'rsync', '-P', htmllogfile, htmlchangelogfile, droiduser + '@' + droidhost + ':' + uploadpath ])
+    subprocess.call(['rsync', '-P', htmllogfile, htmlchangelogfile, droiduser + '@' + droidhost + ':' + uploadpath ])
 # mirror the log files
 if localmirror:
-    subprocess.call([ 'rsync', '-P', logfile, changelogfile, os.path.join(localmirror, 'cron', DATE)])
-
+    subprocess.call(['rsync', '-P', logfile, changelogfile, os.path.join(localmirror, mirrorpath)])
 
 # cd previous working dir
 os.chdir(previous_working_dir)
