1e6360f93eae5150fffc498d6c9039486c484a5c
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-20 23:33:22 -0500

    updatewebsite: change rsync options

--- updatewebsite.sh
+++ updatewebsite.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 # Andrew Sutherland <dr3wsuth3rland@gmail.com>
 
-# update mirror
-rsync -rtP -e 'ssh -p30000' evervolv@217.150.244.124:~/uploads/ /home/drew/uploads/mirror/
+base_dir=/home/drew/uploads
+release_dir=mirror
+nightly_dir=cron
+
+# update release mirror
+rsync -qrpt --partial --delete -e 'ssh -p30000' evervolv@217.150.244.124:~/uploads/ $base_dir/$release_dir/
 
 # update webpages
 WD=$(dirname $0)
-python $WD/releasepage.py /home/drew/uploads/mirror
-python $WD/nightlypage.py /home/drew/uploads/cron
+python $WD/releasepage.py $base_dir/$release_dir
+python $WD/nightlypage.py $base_dir/$nightly_dir
 python $WD/mainpage.py
