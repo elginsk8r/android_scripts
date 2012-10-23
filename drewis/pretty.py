b6e6ae410eae3f7b633107680ec847afe35ffc27
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-23 01:01:06 -0500

    pretty: fix logic

--- drewis/pretty.py
+++ drewis/pretty.py
@@ -3,5 +3,5 @@
 def time(t):
     '''takes timedelta object, returns string'''
     h, r = divmod(t.seconds, 3600)
-    m, s = divmod(t.seconds, 60)
+    m, s = divmod(r, 60)
     return '%sh %sm %ss' % (h, m, s)
