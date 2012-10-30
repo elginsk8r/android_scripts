c77015f681e19266a4400f1b248eeba0e8128340
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-10-29 21:15:11 -0500

    updatewebsite: copy resources

--- updatewebsite.sh
+++ updatewebsite.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # Andrew Sutherland <dr3wsuth3rland@gmail.com>
 
+# Run from website root directory
+
 base_dir=/home/drew/uploads
 release_dir=mirror
 nightly_dir=cron
@@ -8,8 +10,12 @@ nightly_dir=cron
 # update release mirror
 rsync -qrpt --partial -e 'ssh -p30000' evervolv@217.150.244.124:~/uploads/ $base_dir/$release_dir/
 
-# update webpages
 WD=$(dirname $0)
+
+# update webpages
 python $WD/releasepage.py $base_dir/$release_dir
 python $WD/nightlypage.py $base_dir/$nightly_dir
 python $WD/mainpage.py
+
+# copy resources
+rsync -qrpt --partial $WD/website_resources/ .
