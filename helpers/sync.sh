9ab36a7e07139aa6520427238b75fad5eed17017
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-11-29 12:30:19 -0600

    sync: store previous branch in file

--- helpers/sync.sh
+++ helpers/sync.sh
@@ -4,14 +4,21 @@
 get_changelog () {
     local current previous
     current=$(date +%Y.%m.%d)
-    pushd build >/dev/null 2>&1
-    previous=$(git status -bsz)
-    previous=${previous#\#\#\ } # Too hacky?
-    popd >/dev/null 2>&1
-    test "$previous" = "HEAD (no branch)" && return 1
+    if [ -f .previous_branch ]; then
+        previous=$(cat .previous_branch)
+    else
+        pushd build >/dev/null 2>&1
+        previous=$(git status -bsz)
+        previous=${previous#\#\#\ } # Too hacky?
+        popd >/dev/null 2>&1
+        if [ "$previous" = "HEAD (no branch)" ]; then
+            echo "no previous branch found"; return 1
+        fi
+    fi
     echo "${previous}..${current}"
     repo sync -fd -j12 >/dev/null 2>&1 || { echo "Sync failed"; return 1; }
     repo start ${current} --all >/dev/null 2>&1
+    echo $current > .previous_branch
     repo forall -pvc git log --oneline --no-merges ${previous}..${current} 2>/dev/null
     return 0
 }
