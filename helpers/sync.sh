d37de2252bb28da1a218c294ba72604140fb3b32
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-07 21:56:30 -0600

    sync: try and fix the tree on fail

--- helpers/sync.sh
+++ helpers/sync.sh
@@ -1,6 +1,17 @@
 #!/bin/bash
 # Andrew Sutherland <dr3wsuth3rland@gmail.com>
 
+reset_tree () {
+    echo "Sync failed..."
+    echo "Cleaning"
+    repo forall -c git clean -fd >/dev/null 2>&1 || return 1
+    echo "Resetting"
+    repo forall -c git reset --hard >/dev/null 2>&1 || return 1
+    echo "Resyncing"
+    repo sync -fd -j12 >/dev/null || return 1
+    return 0
+}
+
 get_changelog () {
     local current previous
     current=$(date +%Y.%m.%d)
@@ -12,11 +23,18 @@ get_changelog () {
         previous=${previous#\#\#\ } # Too hacky?
         popd >/dev/null 2>&1
         if [ "$previous" = "HEAD (no branch)" ]; then
-            echo "no previous branch found"; return 1
+            echo "No previous branch found"
+            return 1
         fi
     fi
     echo "${previous}..${current}"
-    repo sync -fd -j12 >/dev/null 2>&1 || { echo "Sync failed"; return 1; }
+    repo sync -fd -j12 >/dev/null || reset_tree
+    if [ $? -ne 0 ]; then
+        echo "Sync failed again"
+        echo "Dumping Status:"
+        repo status
+        return 1
+    fi
     repo start ${current} --all >/dev/null 2>&1
     echo $current > .previous_branch
     repo forall -pvc git log --oneline --no-merges ${previous}..${current} 2>/dev/null
