99050d35b2366ec1350d1a635e44039546f463c6
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-11 12:34:31 -0600

    sync: detach current branch after getting changelog
    this way we can manually sync and do builds without upsetting the changelog

--- helpers/sync.sh
+++ helpers/sync.sh
@@ -2,7 +2,7 @@
 # Andrew Sutherland <dr3wsuth3rland@gmail.com>
 
 reset_tree () {
-    echo "Sync failed..."
+    echo "Sync failed!"
     echo "Cleaning"
     repo forall -c git clean -fd >/dev/null 2>&1 || return 1
     echo "Resetting"
@@ -12,32 +12,41 @@ reset_tree () {
     return 0
 }
 
+sync_tree () {
+    repo sync -fd -j12 >/dev/null || reset_tree
+    if [ $? -ne 0 ]; then
+        echo "Sync failed again?"
+        echo "Dumping Status:"
+        repo status
+        return 1
+    fi
+    return 0
+}
+
+update_branch () {
+    local current=$1
+    repo start $current --all >/dev/null 2>&1
+    echo $current > .previous_branch
+    return 0
+}
+
 get_changelog () {
     local current previous
     current=$(date +%Y.%m.%d)
     if [ -f .previous_branch ]; then
         previous=$(cat .previous_branch)
     else
-        pushd build >/dev/null 2>&1
-        previous=$(git status -bsz)
-        previous=${previous#\#\#\ } # Too hacky?
-        popd >/dev/null 2>&1
-        if [ "$previous" = "HEAD (no branch)" ]; then
-            echo "No previous branch found"
-            return 1
-        fi
+        echo "No previous branch!"
+        sync_tree || return 1
+        echo "Init $current as base for next build"
+        update_branch $current
+        return 0
     fi
     echo "${previous}..${current}"
-    repo sync -fd -j12 >/dev/null || reset_tree
-    if [ $? -ne 0 ]; then
-        echo "Sync failed again"
-        echo "Dumping Status:"
-        repo status
-        return 1
-    fi
-    repo start ${current} --all >/dev/null 2>&1
-    echo $current > .previous_branch
+    sync_tree || return 1
+    update_branch $current
     repo forall -pvc git log --oneline --no-merges ${previous}..${current} 2>/dev/null
+    repo sync -fdl -j12 >/dev/null 2>&1
     return 0
 }
 
