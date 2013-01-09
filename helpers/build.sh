9888d224b507ac091c34e17fc85666f1f78f9d8e
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-01-09 16:49:15 -0600

    build: set ceiling and 32 jobs

--- helpers/build.sh
+++ helpers/build.sh
@@ -5,6 +5,7 @@ run_build () {
     local target=$1 args="otapackage"
     local threads=$(($(cat /proc/meminfo | head -n1 | awk '{print $2}')/1000000))
     threads=$(($threads + $(($threads % 2)))) # Round odd number up
+    test $threads -gt 32 && threads=32 # 32 jobs is upper limit
     test "$target" = "passion" && args+=" systemupdatepackage"
     source build/envsetup.sh >/dev/null 2>&1 || return 1
     breakfast $target        >/dev/null 2>&1 || return 1
