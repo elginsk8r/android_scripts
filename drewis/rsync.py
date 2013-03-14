866fdd21635863217686b06e1137afc79f71537e
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-03-14 11:44:46 -0500

    rsync: remove file when were done

--- drewis/rsync.py
+++ drewis/rsync.py
@@ -43,3 +43,5 @@ def rsync(local_file, remote_path, port=None, message='Synced'):
         logging.info("%s %s in %s" % (message,
                     os.path.basename(local_file),
                     pretty.time(datetime.datetime.now() - start)))
+    finally:
+        os.remove(local_file)
