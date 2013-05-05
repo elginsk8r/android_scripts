d34b6b419b6ceee62bf98d647e0bdf1b9084b951
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-05-05 02:44:25 -0500

    removeolddirs: backup dirs first

--- danger/removeolddirs.py
+++ danger/removeolddirs.py
@@ -2,8 +2,10 @@
 '''
     Yanked from stackoverflow
     delete subdirs in directories older than numdays
-    To be used for nightly builds to only keep the latest
+    backs up diretory first
+    To be used for nightly builds to only keep the latest on the site
     arg 1: directory to check
+    arg 2: directory to copy to
 '''
 
 from sys import argv
@@ -13,7 +15,14 @@ import shutil
 
 numdays = 86400*22 # 3 weeks
 now = time.time()
-script, directory = argv
+script, directory, backup_dir = argv
+
+try:
+    subprocess.check_call(['rsync', '-aq', directory, backup_dir])
+except subprocess.CalledProcessError as e:
+    print e
+    exit()
+
 for r,d,f in os.walk(directory):
     for dir in d:
          timestamp = os.path.getmtime(os.path.join(r,dir))
@@ -21,6 +30,6 @@ for r,d,f in os.walk(directory):
              try:
                   print "removing ",os.path.join(r,dir)
                   shutil.rmtree(os.path.join(r,dir))
-             except Exception,e:
+             except Exception as e:
                   print e
                   pass
