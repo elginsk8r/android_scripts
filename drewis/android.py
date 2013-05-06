b56a25acab477bd9260d31c2f03c844b7f2f00df
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-05-05 23:12:07 -0500

    android: raise max_jobs to 32

--- drewis/android.py
+++ drewis/android.py
@@ -48,8 +48,9 @@ def build(target, packages, clobber=True):
     except IOError:
         mem_total = 8199922 # 8GB
 
-    jobs = 24 # Upper limit
-    max_jobs = int(mem_total)/2000000
+    jobs = int((mem_total*0.7)/1000000) # 70% available ram
+    jobs += jobs % 2 # ensure even number
+    max_jobs = 32 # Testing shows more than 32 jobs does not improve build times
     if jobs > max_jobs:
         jobs = max_jobs
 
